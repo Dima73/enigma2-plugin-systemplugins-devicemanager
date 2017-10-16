43cd0fa1f97810a879075ff23ec082f12d4e970e
Dimitrij <Dima-73@inbox.lv>
2017-10-16 14:12:44 +0300

    Add files via upload

--- src/plugin.py
+++ src/plugin.py
@@ -9,15 +9,12 @@ import os
 def supportExtFat():
 	if not os.path.isfile("/sbin/mkexfatfs"):
 		arch = os.popen("uname -m").read()
-		if 'mips' in arch:
+		if 'mips' in arch and os.path.isfile("/usr/lib/enigma2/python/Plugins/SystemPlugins/DeviceManager/bin/mips/mkexfatfs"):
 			os.system("cp /usr/lib/enigma2/python/Plugins/SystemPlugins/DeviceManager/bin/mips/mkexfatfs /sbin/mkexfatfs && chmod 755 /sbin/mkexfatfs && ln /sbin/mkexfatfs /sbin/mkfs.exfat")
 			os.system("cp /usr/lib/enigma2/python/Plugins/SystemPlugins/DeviceManager/bin/mips/exfatfsck /sbin/exfatfsck && chmod 755 /sbin/exfatfsck")
-		elif 'armv7l' in arch:
+		elif 'armv7l' in arch and os.path.isfile("/usr/lib/enigma2/python/Plugins/SystemPlugins/DeviceManager/bin/armv7l/mkexfatfs"):
 			os.system("cp /usr/lib/enigma2/python/Plugins/SystemPlugins/DeviceManager/bin/armv7l/mkexfatfs /sbin/mkexfatfs && chmod 755 /sbin/mkexfatfs && ln /sbin/mkexfatfs /sbin/mkfs.exfat")
 			os.system("cp /usr/lib/enigma2/python/Plugins/SystemPlugins/DeviceManager/bin/armv7l/exfatfsck /sbin/exfatfsck && chmod 755 /sbin/exfatfsck")
-		elif 'sh4' in arch:
-			os.system("cp /usr/lib/enigma2/python/Plugins/SystemPlugins/DeviceManager/bin/sh4/mkexfatfs /sbin/mkexfatfs && chmod 755 /sbin/mkexfatfs && ln /sbin/mkexfatfs /sbin/mkfs.exfat")
-			os.system("cp /usr/lib/enigma2/python/Plugins/SystemPlugins/DeviceManager/bin/sh4/exfatfsck /sbin/exfatfsck && chmod 755 /sbin/exfatfsck")
 	if "exfat-fuse" in open("/etc/filesystems").read():
 		pass
 	else:
